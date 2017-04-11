411cd78ee4f217e72ac4c25c8797fc0298f0b198
Pallavi Shankar <pallavishankar6@gmail.com>
2017-04-10 22:51:55 -0400

    Opens JournalClickActivity when you click on entry in list

--- app/src/main/java/compsci290/edu/duke/everyday/JournalViewHolder.java
+++ app/src/main/java/compsci290/edu/duke/everyday/JournalViewHolder.java
@@ -3,7 +3,8 @@ package compsci290.edu.duke.everyday;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
-
+import android.util.Log;
+import android.content.Intent;
 /**
  * Created by FD on 4/5/2017.
  */
@@ -17,5 +18,14 @@ public class JournalViewHolder extends RecyclerView.ViewHolder{
         super(v);
         title = (TextView) v.findViewById(R.id.view_holder_title);
         journalDate = (TextView) v.findViewById(R.id.view_holder_date);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("hey", "hey");
+                Intent intent = new Intent(v.getContext(), JournalClickActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 }
