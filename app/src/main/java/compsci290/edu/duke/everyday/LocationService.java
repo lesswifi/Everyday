a214fe44ac9ba328cc99a1615d712266aa801e01
Divya Koyyalagunta <Divya@Divyas-MacBook-Pro.local>
2017-04-17 18:48:24 -0400

    added basic location services functionality

---
+++ app/src/main/java/compsci290/edu/duke/everyday/LocationService.java
@@ -0,0 +1,8 @@
+package compsci290.edu.duke.everyday;
+
+/**
+ * Created by Divya on 4/15/17.
+ */
+
+public class LocationService {
+}
--- app/src/main/java/compsci290/edu/duke/everyday/WriteJournalActivity.java
+++ app/src/main/java/compsci290/edu/duke/everyday/WriteJournalActivity.java
@@ -1,18 +1,24 @@
 package compsci290.edu.duke.everyday;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.BreakIterator;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 
@@ -21,7 +27,7 @@ import java.util.GregorianCalendar;
  */
 
 
-public class WriteJournalActivity extends Activity {
+public class WriteJournalActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "NewJournalEntryActivity";
     private static final String REQUIRED = "Required";
@@ -33,10 +39,24 @@ public class WriteJournalActivity extends Activity {
     private EditText mContentField;
     private FloatingActionButton mSubmitButton;
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private BreakIterator mLatitudeText;
+    private BreakIterator mLongitudeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_quiz);
+
+        //create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         // Initialize the DB Reference
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -66,6 +86,8 @@ public class WriteJournalActivity extends Activity {
                 Calendar calendar = GregorianCalendar.getInstance();
                 long date = calendar.getTimeInMillis();
                 submitEntry(title, content, tag, date);
+
+                System.out.println(mLastLocation.toString());
 /**                mDatabase.child("journalentries").addListenerForSingleValueEvent(
                         new ValueEventListener() {
                             @Override
@@ -85,7 +107,20 @@ public class WriteJournalActivity extends Activity {
                 );  **/
             }
         });
+
+
     }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
 
     private void setEditingEnabled(boolean value) {
         mTitleField.setEnabled(value);
@@ -106,6 +141,31 @@ public class WriteJournalActivity extends Activity {
 
         mDatabase.child(key).setValue(journalEntry);
         journalEntry.setJournalId(key);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        try {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+        }
+        catch (SecurityException e) {
+            // lets the user know there is a problem with the gps
+        }
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
 }
