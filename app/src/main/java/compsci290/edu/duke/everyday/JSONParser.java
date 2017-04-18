8d70c2a44b3b37cf6c2de6efd574ddf558b703c8
Divya Koyyalagunta <Divya@Divyas-MacBook-Pro.local>
2017-04-17 22:46:56 -0400

    added location services

---
+++ app/src/main/java/compsci290/edu/duke/everyday/JSONParser.java
@@ -0,0 +1,8 @@
+package compsci290.edu.duke.everyday;
+
+/**
+ * Created by Divya on 4/17/17.
+ */
+
+public class JSONParser {
+}
--- app/src/main/java/compsci290/edu/duke/everyday/WriteJournalActivity.java
+++ app/src/main/java/compsci290/edu/duke/everyday/WriteJournalActivity.java
@@ -1,19 +1,28 @@
 package compsci290.edu.duke.everyday;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -27,7 +36,7 @@ import java.util.GregorianCalendar;
  */
 
 
-public class WriteJournalActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class WriteJournalActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
     private static final String TAG = "NewJournalEntryActivity";
     private static final String REQUIRED = "Required";
@@ -41,9 +50,16 @@ public class WriteJournalActivity extends Activity implements GoogleApiClient.Co
 
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
+    private LocationRequest mLocationRequest;
     private BreakIterator mLatitudeText;
     private BreakIterator mLongitudeText;
 
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+
+    private int MY_PERMISSIONS_REQUEST = 31*5;
+
+    private String weatherRequestURL = "api.openweathermap.org/data/2.5/weather?lat={";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +73,7 @@ public class WriteJournalActivity extends Activity implements GoogleApiClient.Co
                     .addApi(LocationServices.API)
                     .build();
         }
+
 
         // Initialize the DB Reference
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -87,7 +104,7 @@ public class WriteJournalActivity extends Activity implements GoogleApiClient.Co
                 long date = calendar.getTimeInMillis();
                 submitEntry(title, content, tag, date);
 
-                System.out.println(mLastLocation.toString());
+                //System.out.println(mLastLocation.toString());
 /**                mDatabase.child("journalentries").addListenerForSingleValueEvent(
                         new ValueEventListener() {
                             @Override
@@ -145,24 +162,70 @@ public class WriteJournalActivity extends Activity implements GoogleApiClient.Co
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        try {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
+        // Begin polling for new location updates.
+        // Request location updates
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST);
+            }
         }
-        catch (SecurityException e) {
-            // lets the user know there is a problem with the gps
-        }
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                mLocationRequest, this);
+        // Get last known recent location.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        // Note that this can be NULL if last location isn't already known.
         if (mLastLocation != null) {
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            // Print current location if not null
+            Log.d("DEBUG", "current location: " + mLastLocation.toString());
+            LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            weatherRequestURL = weatherRequestURL + mLastLocation.getLatitude() + "}&lon={" +  mLastLocation.getLongitude()+ "}";
         }
+
     }
 
+
+
+
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+    }
     @Override
     public void onConnectionSuspended(int i) {
-
+        if (i == CAUSE_SERVICE_DISCONNECTED) {
+            Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+        } else if (i == CAUSE_NETWORK_LOST) {
+            Toast.makeText(this, "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
+        }
     }
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
