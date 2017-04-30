a70b28abe03908e51beb85496ccdda9ced19dac2
fd53 <fd53@duke.edu>
2017-04-29 22:30:23 -0400

    Added toolbar for Analytics activity, but can't switch between main and analytics

--- MyEveryDay/app/src/main/java/compsci290/edu/duke/myeveryday/AnalyticsActivity.java
+++ MyEveryDay/app/src/main/java/compsci290/edu/duke/myeveryday/AnalyticsActivity.java
@@ -2,8 +2,18 @@ package compsci290.edu.duke.myeveryday;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.util.Pair;
 import android.view.MotionEvent;
@@ -21,6 +31,9 @@ import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PanZoom;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -28,11 +41,27 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import compsci290.edu.duke.myeveryday.Authentication.AuthUiActivity;
 import compsci290.edu.duke.myeveryday.Models.JournalEntry;
+import compsci290.edu.duke.myeveryday.Tag.TagList;
+import compsci290.edu.duke.myeveryday.Tag.TagListFragment;
 import compsci290.edu.duke.myeveryday.util.Constants;
 import compsci290.edu.duke.myeveryday.util.MyXYSeries;
 
@@ -40,7 +69,7 @@ import compsci290.edu.duke.myeveryday.util.MyXYSeries;
 /**
  * Created by Divya on 4/27/17.
  */
-public class AnalyticsActivity extends Activity{
+public class AnalyticsActivity extends AppCompatActivity {
 
     private XYPlot plot;
     private FirebaseAuth mFirebaseAuth;
@@ -56,6 +85,22 @@ public class AnalyticsActivity extends Activity{
     private MyXYSeries series;
 
 
+    private String mUsername;
+    private String mPhotoURL;
+    private String memailaddress;
+
+    private AccountHeader mHeader = null;
+    private Drawer mDrawer = null;
+    private Activity mActivity;
+
+    private static final String ANONYMOUS = "Anonymous";
+    public static final String ANONYMOUS_PHOTO_URL = "https://dl.dropboxusercontent.com/u/15447938/notepadapp/anon_user_48dp.png";
+    public static final String ANONYMOUS_EMAIL = "anonymous@noemail.com";
+
+    @BindView(android.R.id.content) View mRootView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,10 +111,7 @@ public class AnalyticsActivity extends Activity{
         mcloudReference = mdatabase.child(Constants.USERS_CLOUD_END_POINT + mFirebaseUser.getUid() + Constants.NOTE_CLOUD_END_POINT);
         setContentView(R.layout.analytics_plot);
 
-
         plotSetup();
-
-
 
         mJournals = new ArrayList<>();
         mcloudReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -98,7 +140,188 @@ public class AnalyticsActivity extends Activity{
             }
         });
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        mActivity = this;
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if(mFirebaseUser == null)
+        {
+            startActivity(new Intent(this, AuthUiActivity.class));
+            finish();
+            return;
+        }
+        else
+        {
+            //user is logged in
+            mUsername = mFirebaseUser.getDisplayName();
+            //if photourl is not loaded
+            if(mFirebaseUser.getPhotoUrl() != null)
+            {
+                mPhotoURL = mFirebaseUser.getPhotoUrl().toString();
+            }
+            memailaddress = mFirebaseUser.getEmail();
+
+            //String uid = mFirebaseUser.getUid();
+        }
+
+        setnavigationdrawer(savedInstanceState);
+
+    }
+
+    private void setnavigationdrawer(Bundle savedInstanceState) {
+
+        mUsername = TextUtils.isEmpty(mUsername)? ANONYMOUS:mUsername;
+        memailaddress = TextUtils.isEmpty(memailaddress) ? ANONYMOUS_EMAIL : memailaddress;
+        mPhotoURL = TextUtils.isEmpty(mPhotoURL) ? ANONYMOUS_PHOTO_URL : mPhotoURL;
+
+        IProfile profile = new ProfileDrawerItem()
+                .withName(mUsername)
+                .withEmail(memailaddress)
+                .withIcon(mPhotoURL)
+                .withIdentifier(102);
+
+        mHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(profile)
+                .build();
+
+        mDrawer = new DrawerBuilder()
+                .withAccountHeader(mHeader)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Journals").withIcon(GoogleMaterial.Icon.gmd_view_list).withIdentifier(Constants.NOTES),
+                        new PrimaryDrawerItem().withName("Tags").withIcon(GoogleMaterial.Icon.gmd_folder).withIdentifier(Constants.CATEGORIES),
+                        new PrimaryDrawerItem().withName("Analytics").withIcon(GoogleMaterial.Icon.gmd_caret_up_circle).withIdentifier(Constants.ANALYTICS),
+                        new PrimaryDrawerItem().withName("Logout").withIcon(GoogleMaterial.Icon.gmd_lock).withIdentifier(Constants.LOGOUT)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            //String name = ((Nameable) drawerItem).getName().getText(mActivity);
+                            toolbar.setTitle("Tags");
+                        }
+
+                        if (drawerItem != null){
+                            //handle on navigation drawer item
+                            onTouchDrawer((int) drawerItem.getIdentifier());
+                        }
+
+                        return false;
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener()
+                {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        KeyboardUtil.hideKeyboard(AnalyticsActivity.this);
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
+        mDrawer.addStickyFooterItem(new PrimaryDrawerItem().withName("Delete Account!").withIcon(GoogleMaterial.Icon.gmd_delete).withIdentifier(Constants.DELETE));
+    }
+
+    public void onTouchDrawer(int position)
+    {
+        switch (position){
+            case Constants.NOTES:
+                //Do Nothing, we are already on Notes
+                //startActivity(new Intent(AnalyticsActivity.this, MainActivity.class));
+                break;
+            case Constants.CATEGORIES:
+                startActivity(new Intent(AnalyticsActivity.this, TagList.class));
+                break;
+            case Constants.ANALYTICS:
+                break;
+            case Constants.LOGOUT:
+                logout();
+                break;
+            case Constants.DELETE:
+                deleteAccountClicked();
+                break;
+        }
+    }
+
+    public void logout()
+    {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(mActivity, MainActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.sign_out_failed);
+                        }
+                    }
+                });
+
+
+    }
+
+    public void deleteAccountClicked() {
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to delete this account?")
+                .setPositiveButton("Yes, delete it!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteAccount();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .create();
+
+        dialog.show();
+    }
+
+    private void deleteAccount() {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(mActivity, MainActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.delete_account_failed);
+                        }
+                    }
+                });
+    }
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
     }
 
 
